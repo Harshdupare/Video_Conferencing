91009d89415ffddfb128cafb960884790a9c90eb
Harsh <harshdupare2021@gmail.com>
2024-05-04 09:56:00 +0530

    updated the ip address

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,11 +6,11 @@ const Room = () => {
     const { roomId } = useParams<{ roomId: string }>();
     console.log(typeof roomId);
 
-    const name : string | null = localStorage.getItem("Username");
-    console.log(name)
-    if( name == null){
-        return ;
-    }
+    // const name : string | null = localStorage.getItem("Username");
+    // console.log(name)
+    // if( name == null){
+    //     return ;
+    // }
     const mymeeting  = async(element : any) =>{
         if (!roomId) {
             console.error('Room ID is undefined');
@@ -18,7 +18,7 @@ const Room = () => {
         }
         const appID = 309209290 ;
         const serverSecret = "9851e888c9f7fcf84e2edb55bb98ff49";
-        const kitToken : string =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId ,Date.now().toString(), name );
+        const kitToken : string =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId ,Date.now().toString(), "your name" );
         
         const zp = ZegoUIKitPrebuilt.create(kitToken);
         // start the call
